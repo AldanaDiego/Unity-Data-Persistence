8af222169a09d1c9401e82490f3c52029c27805e
unknown <daldana9@gmail.com>
2022-03-16 12:00:12 -0300

    Adjust ball speed, reload bricks after clearing them all

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     private Rigidbody m_Rigidbody;
+    private float maxSpeed;
+    private bool reachedMaxSpeed;
 
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        maxSpeed = 2.7f;
+        reachedMaxSpeed = false;
     }
     
     private void OnCollisionExit(Collision other)
@@ -20,17 +24,37 @@ public class Ball : MonoBehaviour
         velocity += velocity.normalized * 0.01f;
         
         //check if we are not going totally vertically as this would lead to being stuck, we add a little vertical force
-        if (Vector3.Dot(velocity.normalized, Vector3.up) < 0.1f)
-        {
-            velocity += velocity.y > 0 ? Vector3.up * 0.5f : Vector3.down * 0.5f;
+        float verticallity = Vector3.Dot(velocity.normalized, Vector3.up);
+        if (other.gameObject.tag == "Paddle") {
+            if (verticallity > 0.98f)
+            {
+                velocity += new Vector3(GetRandomForce(velocity.normalized.x > 0), 0, 0);
+                
+            } else if (verticallity < 0.15f) {
+                velocity += new Vector3(0, GetRandomForce(true), 0);
+            }
         }
 
         //max velocity
-        if (velocity.magnitude > 3.0f)
+        if (velocity.magnitude > maxSpeed)
         {
-            velocity = velocity.normalized * 3.0f;
+            if (!reachedMaxSpeed) {
+                reachedMaxSpeed = true;
+            }
+            velocity = velocity.normalized * maxSpeed;
         }
 
         m_Rigidbody.velocity = velocity;
+    }
+
+    public void AddMaxSpeed()
+    {
+        this.maxSpeed += 0.3f;
+        this.reachedMaxSpeed = false;
+    }
+
+    private float GetRandomForce(bool positive)
+    {
+        return UnityEngine.Random.Range(0.25f, 0.5f) * (positive ? 1 : -1);
     }
 }
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -16,6 +16,7 @@ public class MainManager : MonoBehaviour
     
     private bool m_Started = false;
     private int m_Points;
+    private int brickCount;
     
     private bool m_GameOver = false;
 
@@ -23,21 +24,7 @@ public class MainManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
-        {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
-        }
-
+        StartCoroutine(FillBoard());
         BestScoreText.text = TopScoreManager.Instance.GetBestScore();
     }
 
@@ -69,11 +56,36 @@ public class MainManager : MonoBehaviour
     {
         m_Points += point;
         ScoreText.text = $"Score : {m_Points}";
+        brickCount--;
+        if (brickCount <= 0) {
+            Ball.gameObject.GetComponent<Ball>().AddMaxSpeed();
+            StartCoroutine(FillBoard());
+        }
     }
 
     public void GameOver()
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
+    }
+
+    private IEnumerator FillBoard()
+    {
+        yield return new WaitUntil(() => Ball.gameObject.transform.position.y <= 2.0f);
+        const float step = 0.6f;
+        int perLine = Mathf.FloorToInt(4.0f / step);
+        int[] pointCountArray = new [] {1,1,2,2,5,5};
+        brickCount = 0;
+        for (int i = 0; i < LineCount; ++i)
+        {
+            for (int x = 0; x < perLine; ++x)
+            {
+                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+                brick.PointValue = pointCountArray[i];
+                brick.onDestroyed.AddListener(AddPoint);
+                brickCount++;
+            }
+        }
     }
 }
